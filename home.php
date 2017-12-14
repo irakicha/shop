c42912a5167e9443e6317bc1013b0ac25248db31
irakicha <irakicha@gmail.com>
2017-12-15 01:15:42 +0200

    fix some bug in home page

--- home.php
+++ home.php
@@ -31,124 +31,124 @@
 									<p>At Peugeot we pride ourselves on innovation. Award-winning PureTech 3-cylinder engine technology is just another example of this. PureTech provides the drive and performance normally associated with bigger engines but with significantly improved fuel consumption.</p>
 								</div>
 							<div class="row align-items-center product-details">
-								<div class="col product-price">
-									<div>650000<span class="product-price_currency">USD</span></div>
+								<div class="col-6 product-price">
+									650000<span class="product-price_currency">USD</span>
 								</div>
-								<div class="col btn-container">
+								<div class="col-6 btn-container">
 									<button class="btn add-to-cart">Add To Cart</button>
 								</div>
 							</div>   <!-- product-item-description  -->
 						</div>  <!-- product -->
-						<div class="col-12 col-md-5 col-lg-3 product">
-							<div class="product-img">
-								<a href="" class="product-link">
-									<img src="images/dummy_img.jpeg" alt="">
-								</a>
-							</div>
+                        <div class="col-12 col-md-5 col-lg-3 product">
+                            <div class="product-img">
+                                <a href="" class="product-link">
+                                    <img src="images/dummy_img.jpeg" alt="">
+                                </a>
+                            </div>
 
-							<h3 class="product-title">
-								<a href="#" class="product-item_link">Peugeot 3008</a>
-							</h3>
-							<div class="product-description">
-								<p>At Peugeot we pride ourselves on innovation. Award-winning PureTech 3-cylinder engine technology is just another example of this. PureTech provides the drive and performance normally associated with bigger engines but with significantly improved fuel consumption.</p>
-							</div>
-							<div class="row align-items-center product-details">
-								<div class="col product-price">
-									<div>650000<span class="product-price_currency">USD</span></div>
-								</div>
-								<div class="col btn-container">
-									<button class="btn add-to-cart">Add To Cart</button>
-								</div>
-							</div>   <!-- product-item-description  -->
-						</div>  <!-- product -->
-						<div class="col-12 col-md-5 col-lg-3 product">
-							<div class="product-img">
-								<a href="" class="product-link">
-									<img src="images/dummy_img.jpeg" alt="">
-								</a>
-							</div>
+                            <h3 class="product-title">
+                                <a href="#" class="product-item_link">Peugeot 3008</a>
+                            </h3>
+                            <div class="product-description">
+                                <p>At Peugeot we pride ourselves on innovation. Award-winning PureTech 3-cylinder engine technology is just another example of this. PureTech provides the drive and performance normally associated with bigger engines but with significantly improved fuel consumption.</p>
+                            </div>
+                            <div class="row align-items-center product-details">
+                                <div class="col-6 product-price">
+                                    650000<span class="product-price_currency">USD</span>
+                                </div>
+                                <div class="col-6 btn-container">
+                                    <button class="btn add-to-cart">Add To Cart</button>
+                                </div>
+                            </div>   <!-- product-item-description  -->
+                        </div>  <!-- product -->
+                        <div class="col-12 col-md-5 col-lg-3 product">
+                            <div class="product-img">
+                                <a href="" class="product-link">
+                                    <img src="images/dummy_img.jpeg" alt="">
+                                </a>
+                            </div>
 
-							<h3 class="product-title">
-								<a href="#" class="product-item_link">Peugeot 3008</a>
-							</h3>
-							<div class="product-description">
-								<p>At Peugeot we pride ourselves on innovation. Award-winning PureTech 3-cylinder engine technology is just another example of this. PureTech provides the drive and performance normally associated with bigger engines but with significantly improved fuel consumption.</p>
-							</div>
-							<div class="row align-items-center product-details">
-								<div class="col product-price">
-									<div>650000<span class="product-price_currency">USD</span></div>
-								</div>
-								<div class="col btn-container">
-									<button class="btn add-to-cart">Add To Cart</button>
-								</div>
-							</div>   <!-- product-item-description  -->
-						</div>  <!-- product -->
-						<div class="col-12 col-md-5 col-lg-3 product">
-							<div class="product-img">
-								<a href="" class="product-link">
-									<img src="images/dummy_img.jpeg" alt="">
-								</a>
-							</div>
+                            <h3 class="product-title">
+                                <a href="#" class="product-item_link">Peugeot 3008</a>
+                            </h3>
+                            <div class="product-description">
+                                <p>At Peugeot we pride ourselves on innovation. Award-winning PureTech 3-cylinder engine technology is just another example of this. PureTech provides the drive and performance normally associated with bigger engines but with significantly improved fuel consumption.</p>
+                            </div>
+                            <div class="row align-items-center product-details">
+                                <div class="col-6 product-price">
+                                    650000<span class="product-price_currency">USD</span>
+                                </div>
+                                <div class="col-6 btn-container">
+                                    <button class="btn add-to-cart">Add To Cart</button>
+                                </div>
+                            </div>   <!-- product-item-description  -->
+                        </div>  <!-- product -->
+                        <div class="col-12 col-md-5 col-lg-3 product">
+                            <div class="product-img">
+                                <a href="" class="product-link">
+                                    <img src="images/dummy_img.jpeg" alt="">
+                                </a>
+                            </div>
 
-							<h3 class="product-title">
-								<a href="#" class="product-item_link">Peugeot 3008</a>
-							</h3>
-							<div class="product-description">
-								<p>At Peugeot we pride ourselves on innovation. Award-winning PureTech 3-cylinder engine technology is just another example of this. PureTech provides the drive and performance normally associated with bigger engines but with significantly improved fuel consumption.</p>
-							</div>
-							<div class="row align-items-center product-details">
-								<div class="col product-price">
-									<div>650000<span class="product-price_currency">USD</span></div>
-								</div>
-								<div class="col btn-container">
-									<button class="btn add-to-cart">Add To Cart</button>
-								</div>
-							</div>   <!-- product-item-description  -->
-						</div>  <!-- product -->
-						<div class="col-12 col-md-5 col-lg-3 product">
-							<div class="product-img">
-								<a href="" class="product-link">
-									<img src="images/dummy_img.jpeg" alt="">
-								</a>
-							</div>
+                            <h3 class="product-title">
+                                <a href="#" class="product-item_link">Peugeot 3008</a>
+                            </h3>
+                            <div class="product-description">
+                                <p>At Peugeot we pride ourselves on innovation. Award-winning PureTech 3-cylinder engine technology is just another example of this. PureTech provides the drive and performance normally associated with bigger engines but with significantly improved fuel consumption.</p>
+                            </div>
+                            <div class="row align-items-center product-details">
+                                <div class="col-6 product-price">
+                                    650000<span class="product-price_currency">USD</span>
+                                </div>
+                                <div class="col-6 btn-container">
+                                    <button class="btn add-to-cart">Add To Cart</button>
+                                </div>
+                            </div>   <!-- product-item-description  -->
+                        </div>  <!-- product -->
+                        <div class="col-12 col-md-5 col-lg-3 product">
+                            <div class="product-img">
+                                <a href="" class="product-link">
+                                    <img src="images/dummy_img.jpeg" alt="">
+                                </a>
+                            </div>
 
-							<h3 class="product-title">
-								<a href="#" class="product-item_link">Peugeot 3008</a>
-							</h3>
-							<div class="product-description">
-								<p>At Peugeot we pride ourselves on innovation. Award-winning PureTech 3-cylinder engine technology is just another example of this. PureTech provides the drive and performance normally associated with bigger engines but with significantly improved fuel consumption.</p>
-							</div>
-							<div class="row align-items-center product-details">
-								<div class="col product-price">
-									<div>650000<span class="product-price_currency">USD</span></div>
-								</div>
-								<div class="col btn-container">
-									<button class="btn add-to-cart">Add To Cart</button>
-								</div>
-							</div>   <!-- product-item-description  -->
-						</div>  <!-- product -->
-						<div class="col-12 col-md-5 col-lg-3 product">
-							<div class="product-img">
-								<a href="" class="product-link">
-									<img src="images/dummy_img.jpeg" alt="">
-								</a>
-							</div>
+                            <h3 class="product-title">
+                                <a href="#" class="product-item_link">Peugeot 3008</a>
+                            </h3>
+                            <div class="product-description">
+                                <p>At Peugeot we pride ourselves on innovation. Award-winning PureTech 3-cylinder engine technology is just another example of this. PureTech provides the drive and performance normally associated with bigger engines but with significantly improved fuel consumption.</p>
+                            </div>
+                            <div class="row align-items-center product-details">
+                                <div class="col-6 product-price">
+                                    650000<span class="product-price_currency">USD</span>
+                                </div>
+                                <div class="col-6 btn-container">
+                                    <button class="btn add-to-cart">Add To Cart</button>
+                                </div>
+                            </div>   <!-- product-item-description  -->
+                        </div>  <!-- product -->
+                        <div class="col-12 col-md-5 col-lg-3 product">
+                            <div class="product-img">
+                                <a href="" class="product-link">
+                                    <img src="images/dummy_img.jpeg" alt="">
+                                </a>
+                            </div>
 
-							<h3 class="product-title">
-								<a href="#" class="product-item_link">Peugeot 3008</a>
-							</h3>
-							<div class="product-description">
-								<p>At Peugeot we pride ourselves on innovation. Award-winning PureTech 3-cylinder engine technology is just another example of this. PureTech provides the drive and performance normally associated with bigger engines but with significantly improved fuel consumption.</p>
-							</div>
-							<div class="row align-items-center product-details">
-								<div class="col product-price">
-									<div>650000<span class="product-price_currency">USD</span></div>
-								</div>
-								<div class="col btn-container">
-									<button class="btn add-to-cart">Add To Cart</button>
-								</div>
-							</div>   <!-- product-item-description  -->
-						</div>  <!-- product -->
+                            <h3 class="product-title">
+                                <a href="#" class="product-item_link">Peugeot 3008</a>
+                            </h3>
+                            <div class="product-description">
+                                <p>At Peugeot we pride ourselves on innovation. Award-winning PureTech 3-cylinder engine technology is just another example of this. PureTech provides the drive and performance normally associated with bigger engines but with significantly improved fuel consumption.</p>
+                            </div>
+                            <div class="row align-items-center product-details">
+                                <div class="col-6 product-price">
+                                    650000<span class="product-price_currency">USD</span>
+                                </div>
+                                <div class="col-6 btn-container">
+                                    <button class="btn add-to-cart">Add To Cart</button>
+                                </div>
+                            </div>   <!-- product-item-description  -->
+                        </div>  <!-- product -->
 					</section> <!--  product-gallery -->
 
 					<section class="column pagination-container">
